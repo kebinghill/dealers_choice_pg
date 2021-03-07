f7407176dcee7c76ce9f16a6b852a27633043e4f
Kevin Gil <93kevingil@gmail.com>
2021-03-06 20:58:35 -0500

    completed hw

--- db.js
+++ db.js
@@ -1,5 +1,51 @@
-const syncAndSeed = async () => {};
+const pg = require('pg');
+const db = new pg.Client('postgres://localhost/dc_pg_db');
+
+const syncAndSeed = async () => {
+  const SQL = `
+  DROP TABLE IF EXISTS console;
+  DROP TABLE IF EXISTS company;
+   CREATE TABLE company(
+     id INTEGER PRIMARY KEY,
+     name VARCHAR(10) NOT NULL
+   );
+   CREATE TABLE console(
+     id INTEGER PRIMARY KEY,
+     name VARCHAR(50) NOT NULL,
+     release_year INTEGER,
+     company_id INTEGER REFERENCES company(id)
+   );
+   INSERT INTO company(id, name) VALUES(1, 'Sony');
+   INSERT INTO company(id, name) VALUES(2, 'Microsoft');
+   INSERT INTO company(id, name) VALUES(3, 'Nintendo');
+   INSERT INTO company(id, name) VALUES(4, 'Atari');
+   INSERT INTO company(id, name) VALUES(5, 'Sega');
+
+   INSERT INTO console(id, name, release_year, company_id) VALUES(1, 'genesis', 1989, 5);
+   INSERT INTO console(id, name, release_year, company_id) VALUES(2, '2600', 1977, 4);
+   INSERT INTO console(id, name, release_year, company_id) VALUES(3, 'switch', 2019, 3);
+   INSERT INTO console(id, name, release_year, company_id) VALUES(4, 'wii', 2006, 3);
+   INSERT INTO console(id, name, release_year, company_id) VALUES(5, 'PS5', 2020, 1);
+   INSERT INTO console(id, name, release_year, company_id) VALUES(6, 'PS1', 1994, 1);
+   INSERT INTO console(id, name, release_year, company_id) VALUES(7, 'Xbox 360', 2005, 2);
+
+  `;
+  await db.query(SQL);
+};
+
+const connect = async () => {
+  try {
+    await db.connect();
+    console.log('connected to DB');
+    await syncAndSeed();
+    console.log('seeded to DB');
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
+  db,
   syncAndSeed,
+  connect,
 };
--- server.js
+++ server.js
@@ -1,22 +1,98 @@
-const pg = require('pg');
 const express = require('express');
-const { syncAndSeed } = require('./db');
-const db = new pg.Client('postgres://localhost/dc_pg_db');
+const { connect, db, syncAndSeed } = require('./db.js');
+const path = require('path');
 
 const app = express();
 
-app.get('/', async (req, res) => {
-  res.send('hello world');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (req, res, next) => {
+  try {
+    const response = await db.query('SELECT * FROM company;');
+    const companies = response.rows;
+
+    res.send(`
+      <html>
+        <head>
+        <link rel='stylesheet' href='/public/style.css'/>
+        </head>
+        <body>
+          <h1>Console Wars</h1>
+          <ul>
+            ${companies
+              .map(
+                (company) => `
+
+            <li>
+            <a href='/companies/${company.id}'>
+            ${company.name}
+            </a>
+            </li>
+            `
+              )
+              .join('')}
+          </ul>
+        </body>
+      </html>
+    `);
+  } catch (err) {
+    next();
+  }
+});
+
+app.get('/companies/:id', async (req, res, next) => {
+  try {
+    const response = await db.query(
+      'SELECT * FROM console WHERE company_id=$1;',
+      [req.params.id]
+    );
+    const responseTwo = await db.query('SELECT * FROM company WHERE id=$1;', [
+      req.params.id,
+    ]);
+    const company = responseTwo.rows;
+    const consoles = response.rows;
+
+    res.send(`
+      <html>
+        <head>
+        <link rel='stylesheet' href='/public/style.css'/>
+        </head>
+        <body>
+        ${company.map(
+          (company) => `
+        <h1><a href='/'>${company.name} Consoles</a></h1>
+        `
+        )}
+          <ul>
+            ${consoles
+              .map(
+                (console) => `
+
+            <li>
+            <a href='/'>
+            ${console.name}
+            </a>
+            <p>released in: ${console.release_year}</p>
+            </li>
+            `
+              )
+              .join('')}
+          </ul>
+        </body>
+      </html>
+    `);
+  } catch (err) {
+    next();
+  }
 });
 
 const init = async () => {
   try {
-    await db.connect();
+    connect();
     const port = process.env.PORT || 3000;
     app.listen(port, () => console.log(`listening on port: ${port}`));
   } catch (error) {
     console.log(error);
-    ß;
   }
 };
 
